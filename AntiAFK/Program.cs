1f48b9a307af59820028b93a3d44cecbb6d143ce
Derek <substance@live.ca>
2019-02-12 21:33:58 -0500

    Current Release

--- AntiAFK/Program.cs
+++ AntiAFK/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Threading;
 using System.Runtime.InteropServices;
 
+using AntiAFK.Utils;
 
 namespace AntiAFK
 {
@@ -61,6 +62,34 @@ namespace AntiAFK
         }
     }
 
+    namespace Utils
+    {
+        struct RandomRange
+        {
+            public RandomRange(int min, int max)
+            {
+                this.min = Math.Abs(min <= max ? min : max);
+                this.max = Math.Abs(max >= min ? max : min);
+
+                rand = new Random(DateTime.Now.Millisecond);
+            }
+
+            public int Next()
+            {
+                if (rand == null)
+                {
+                    rand = new Random(DateTime.Now.Millisecond);
+                }
+
+                return rand.Next(min, max);
+            }
+
+            private Random rand;
+            public readonly int min;
+            public readonly int max;
+        }
+    }
+
     class Program
     {
         enum ProcessStatus
@@ -70,12 +99,19 @@ namespace AntiAFK
 
         private const string _PROCESS_NAME = "World of Warcraft";
         private const string _APP_NAME = "AntiAFK";
+        private static RandomRange _THREAD_DELAY_RANGE = new RandomRange(0, 600000);
+
         private static IntPtr _PROCESS_HWND = IntPtr.Zero;
 
         private static Mutex _MUT = new Mutex();
         private static Thread _THREAD = null;
-        private const int _THREAD_DELAY = 30;
         private static volatile ProcessStatus _STATUS = ProcessStatus.NotFound;
+
+        private static uint [] _KEYS = {
+                0x20,       // space
+                0x33,       // 3
+                0x51        // q
+        };
 
         static void Main(string[] args)
         {
@@ -90,7 +126,7 @@ namespace AntiAFK
                 // TODO: BACKGROUND THREAD FOR PROCESSING
                 _THREAD = new Thread(BackProcess);
                 _THREAD.IsBackground = true;
-                _THREAD.Start(_THREAD_DELAY);
+                _THREAD.Start(_THREAD_DELAY_RANGE);
 
                 // INPUT THREAD
                 while (true)
@@ -128,48 +164,33 @@ namespace AntiAFK
         static void BackProcess(object delay)
         {
             IntPtr consoleWindow = Win32.Imports.FindWindowByCaption(IntPtr.Zero, _APP_NAME);
+            
+            RandomRange keyIndexRange = new RandomRange(0, _KEYS.Length);
+            RandomRange delayRange = delay.GetType() == typeof(RandomRange) ? (RandomRange)delay : new RandomRange(0, 0);
 
-            uint vk_A = 0x41, vk_D = 0x44;
-            uint msg_KeyDown = 0x0100, msg_KeyUp = 0x0101;
-            const uint FRAME_RATE = 60;
-            const uint TIME = FRAME_RATE*60;
-            uint counter = 0;
-
-            bool left = true;
+            const uint msg_KeyDown = 0x0100, msg_KeyUp = 0x0101;
 
             while (_STATUS == ProcessStatus.Found)
             {
                 _PROCESS_HWND = Win32.Imports.FindWindowByCaption(IntPtr.Zero, _PROCESS_NAME);
 
                 // TODO: user can add in new keys, or delete. and can set associated timers with each key.
-                if (_PROCESS_HWND != IntPtr.Zero && delay.GetType() == typeof(int))
+                if (_PROCESS_HWND != IntPtr.Zero)
                 {
-                    //Win32.Imports.SwitchToThisWindow(_PROCESS_HWND, 1);
+                    Thread.Sleep(delayRange.Next());
                     {
                         // TODO: MAIN PROCESSING GOES HERE
-                        if ((counter%TIME) == 0)
-                        {
-                            
-                            uint vKey = left ? vk_A : vk_D;
-                            uint sKey = Win32.Imports.MapVirtualKeyA(vKey, 0);
+                        int keyIndex = keyIndexRange.Next();
+                        Console.WriteLine(keyIndex);
 
-                            Win32.Imports.SendNotifyMessageA(_PROCESS_HWND, msg_KeyUp, vKey, 0x00000000 | (sKey << 16)
-                                | 0x00000000 | 0x00000000 | 0x00000000);
+                        uint vKey = _KEYS[keyIndex];
+                        uint sKey = Win32.Imports.MapVirtualKeyA(vKey, 0);
 
-                            left = !left;
-                            vKey = left ? vk_A : vk_D;
-                            sKey = Win32.Imports.MapVirtualKeyA(vKey, 0);
-                            Win32.Imports.SendNotifyMessageA(_PROCESS_HWND, msg_KeyDown, vKey, 0x00000000 | (sKey << 16)
+                        Win32.Imports.SendNotifyMessageA(_PROCESS_HWND, msg_KeyDown, vKey, 0x00000000 | (sKey << 16)
                                 | 0x00000000 | 0x00000000 | 0x00000000);
-                        }
-                        
-                        //Win32.Imports.SendMessage(_PROCESS_HWND, msg, vKey, 0x00000000 | (sKey << 16)
-                        //    | 0x00000000 | 0x00000000 | 0x00000000);
+                        Win32.Imports.SendNotifyMessageA(_PROCESS_HWND, msg_KeyUp, vKey, 0x00000000 | (sKey << 16)
+                                | 0x00000000 | 0x00000000 | 0x00000000);
                     }
-                    //Win32.Imports.SwitchToThisWindow(consoleWindow, 1);
-
-                    ///Thread.Sleep((int)delay);
-                    ++counter;
                 }
                 else
                 {
